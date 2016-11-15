628be4ac0c858fedc2b91c6f0f513dbd6620b387
Tyler Kershner <tylerkershner@gmail.com>
2016-11-14 23:07:06 -0500

    Small logic tweaks

--- app/modules/startup.py
+++ app/modules/startup.py
@@ -24,20 +24,21 @@ class StartupThread(threading.Thread):
             # Check For DLL error
             if not utility.dll_check():
                 url = base_url + 'dll-error'
-            # Check to see if config needs to be updated
-            if not utility.config_check():
-                url = base_url + 'update-config'
             # Check if config file has been created yet
             elif os.path.isfile(utility.get_config_path()):
-                config = ConfigParser.RawConfigParser()
-                config.read(utility.get_config_path())
-                utility.write_config('App State', 'running', '0')
+                # Check to see if config needs to be updated
+                if not utility.config_check():
+                    url = base_url + 'update-config'
+                else:
+                    config = ConfigParser.RawConfigParser()
+                    config.read(utility.get_config_path())
+                    utility.write_config('App State', 'running', '0')
 
-                # Wait for 200 status code from server then load up interface
-                while not utility.check_server(self.host):
-                    sleep(0.2)
+                    # Wait for 200 status code from server then load up interface
+                    while not utility.check_server(self.host):
+                        sleep(0.2)
 
-                sb_controller.start()
+                    sb_controller.start()
             else:
                 # Config file doesn't exist, open New User interface
                 print 'Redirecting to New User interface...'
